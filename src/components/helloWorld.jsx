95abc03cc1ce171509aa56a301210f718c941d3b
Ingrid Taroy <i.taroy@gmail.com>
2016-04-12 10:43:07 +0200

    en funksjon

--- src/components/helloWorld.jsx
+++ src/components/helloWorld.jsx
@@ -3,14 +3,25 @@ import React, { PropTypes } from 'react'
 export default class HelloWorld extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            enTilfeldigState: false
+        }
+    }
+
+    enTilfeldigfunksjon() {
+        console.log("En funksjon")
     }
 
     render() {
         let {name} = this.props
+        
+        this.enTilfeldigfunksjon()
+        
         return (
         	<div className="helloworld">
             	<h1>Hello {name} </h1>
-            	<p> This is a small web app </p>
+            	<p> This is a very small web app </p>
         	</div>
         ) 
     }
